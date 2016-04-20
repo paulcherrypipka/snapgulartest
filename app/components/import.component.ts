c4987a8efe658b0396ec0be6a2f80c31d27728e2
Paul <Paul@test.local>
2016-04-20 14:03:40 +0300

    implements Import from archive - require data.json file in it

--- app/components/import.component.ts
+++ app/components/import.component.ts
@@ -34,15 +34,49 @@ export class ImportComponent implements OnInit {
     }
 
     selectImportFile(event: any) {
+
+        // @todo implements - import from archive with IMAGES
         if (event.srcElement.files[0] instanceof File) {
-            console.log('Selected file => ', event.srcElement.files[0]);
-            let FR = new FileReader();
-            FR.onload = (e) => {
-                //noinspection TypeScriptUnresolvedVariable
-                this.selectedImportFileContent = e.target.result;
-            };
-            FR.readAsText(event.srcElement.files[0]);
-            this.isValid = true;
+
+            // Get extension helper
+            let getExtension = (filename) => { return filename.split('.').pop() };
+            if (getExtension(event.srcElement.files[0].name) == "zip") {
+
+                console.log('IMport components getting ZIP archive');
+
+                let FR = new FileReader();
+                //noinspection TypeScriptUnresolvedFunction
+                FR.onload = (e) => {
+
+                    // !!!WARNING!!! - use jszip v.2.6.0 (version) npm package
+                    //noinspection TypeScriptUnresolvedVariable,TypeScriptUnresolvedFunction
+                    let zip = new JSZip(e.target.result);
+                    for (let filename in zip.files) {
+                        let file = zip.files[filename];
+
+                        console.log('Checked filename => ', filename);
+
+                        if (filename == "data.json") {
+
+                            console.log('Found data.json file in archive');
+
+                            this.selectedImportFileContent = file.asText();
+                            this.isValid = true;
+                        }
+                    }
+                };
+                //FR.readAsBinaryString(event.srcElement.files[0]);
+                FR.readAsBinaryString(event.srcElement.files[0]);
+            }
+            if (getExtension(event.srcElement.files[0].name) == "json") {
+                let FR = new FileReader();
+                FR.onload = (e) => {
+                    //noinspection TypeScriptUnresolvedVariable
+                    this.selectedImportFileContent = e.target.result;
+                };
+                FR.readAsText(event.srcElement.files[0]);
+                this.isValid = true;
+            }
         }
     }
 
